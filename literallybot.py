76fe21ea3f549a3f7abdd90bfbba50ce04a833d8
Joel Guerra <joelaguerra1@gmail.com>
2017-08-13 15:21:57 -0700

    fixes bot replying to itself

--- literallybot.py
+++ literallybot.py
@@ -2,7 +2,7 @@ from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
 import re
-import requests
+# import requests
 import bs4
 import praw
 import time
@@ -10,43 +10,49 @@ import time
 
 history = 'commented.txt'
 
-reply = 'Did you really mean to say literally?\nConsider reading http://writingexplained.org/literally-vs-figuratively-difference to learn when to use the word "literally".'
+reply = "Did you really mean to say literally?\nConsider reading [this](http://writingexplained.org/literally-vs-figuratively-difference) to make sure you're using 'literally' correctly."
 
 def authenticate():
 
     print('Authenticating...\n')
-    reddit = praw.Reddit('literallybot', user_agent = '/u/LiterallyLiteralBot')
+    reddit = praw.Reddit('literallybot', user_agent = '/u/LiteralLiterallyBot')
     print('Authenticated as {}\n'.format(reddit.user.me()))
     return reddit
 
 
 def literallybot(reddit):
 
-    print("Getting 100 comments...\n")
+    print("Getting 500 comments...\n")
 
-    for comment in reddit.subreddit('test').comments(limit = 100):
+    for comment in reddit.subreddit('test').comments(limit = 500):
         match = re.findall("literally", comment.body)
         if match:
             print("'Literally' found in comment with comment ID: " + comment.id)
 
             file_obj_r = open(history,'r')
             if comment.id not in file_obj_r.read().splitlines():
-                print('Literally a new post. Commenting on:')
-                print(comment.body)
-                comment.reply(reply)
+                if comment.author.name == reddit.user.me():
+                    print('     Skipping my own comment...\n')
+                    file_obj_r.close()
+                    file_obj_w = open(history,'a+')
+                    file_obj_w.write(comment.id + '\n')
+                    file_obj_w.close()
+                else:
+                    print('     Found new comment by ' + comment.author.name + '\n')
+                    comment.reply(reply)
 
-                file_obj_r.close()
-
-                file_obj_w = open(history,'a+')
-                file_obj_w.write(comment.id + '\n')
-                file_obj_w.close()
-                time.sleep(600)
+                    file_obj_r.close()
+                    file_obj_w = open(history,'a+')
+                    file_obj_w.write(comment.id + '\n')
+                    file_obj_w.close()
+                    print('Waiting 10 minutes before commenting again')
+                    time.sleep(600)
             else:
-                print('Already commented on this!\n')
+                print('     Already commented on this!\n')
 
             time.sleep(10)
 
-    print('Waiting 30 seconds...\n')
+    print('Waiting 30 seconds before pulling more comments...\n')
     time.sleep(30)
 
 
